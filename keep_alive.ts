3b787b194e43cf08fa8370ef535feee0e8cd491b
amirking1277600-star <amirking1277600@gmail.com>
2026-06-09 15:35:14 +0300

    Update keep_alive.ts

--- keep_alive.ts
+++ keep_alive.ts
@@ -2,11 +2,11 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Bot is active and running 24/7');
+    res.send('Server is active');
 });
 
 export function keepAlive() {
     app.listen(3000, () => {
-        console.log('Keep-alive server is running on port 3000');
+        console.log('Keep-alive server is running');
     });
 }
